1446844a62af93319c77106029aeb980d60cd045
Rachit2903 <97540447+Rachit2903@users.noreply.github.com>
2023-07-08 23:17:34 +0530

    Update game.js

--- game.js
+++ game.js
@@ -53,7 +53,7 @@ function checkAnswer(currentLevel){
         setTimeout(function(){
             $("body").removeClass("game-over")
         },200);
-        $("#level-title").text("Game Over, Press Any Key to Restart");
+        $("#level-title").text("Game Over, Press Button to Restart");
         startOver();
     }
 }
